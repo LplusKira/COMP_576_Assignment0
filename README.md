35980b6ec472e0d1f7d57df0237b45960c137627
LplusKira <pokaichangtwn@gmail.com>
2020-09-07 19:08:11 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Synopsis
+It's a simple demo repo for COMP 576's assinment 0.
---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_rrr(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'RRR, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_rrr('Task 6 of Assignment 0 of ELEC/COMP 576: Introduction to Deep Learning')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
